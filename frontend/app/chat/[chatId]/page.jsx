7e2816b66c84589c5003863027a500e10a6ecb67
Sash107 <suyashupadhyay2004@gmail.com>
2026-02-26 16:46:46 +0530

    done with chadId

--- frontend/app/chat/[chatId]/page.jsx
+++ frontend/app/chat/[chatId]/page.jsx
@@ -1,22 +1,60 @@
 'use client';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Sidebar from '../../../components/Sidebar'
 import { useParams } from 'next/navigation';
 import axios from 'axios';
+import ReactMarkdown from "react-markdown";
+import Chatbox from '../../../components/Chatbox';
 
-const ChatPage = ({params}) => {
+const ChatPage = () => {
     const {chatId}=useParams();
-    const [messages,setMessages]=useState([]);
-    const url = "http://localhost:3000/api/chat/all-message-in-conversation/" + chatId
+    const [Allmessages,setAllMessages]=useState([]);
+    const [isLoading,setIsLoading]=useState(false);
+    const messageEndRef=useRef(null);
 
+    const handleSend=async (msg)=>{
+        const trimmed=msg.trim()
+
+        if(!trimmed)return
+        if(isLoading)return
+
+        const url="http://localhost:3000/api/chat/send-message"
+
+        setAllMessages((prev)=>{
+            return [...(prev || []), {
+                content: msg,
+                createdAt:Date.now(),
+                role: "user"
+            }]
+        })
+        try{
+            setIsLoading(true);
+            const response=await axios.post(url,{"message":msg,"conversationId":chatId},{withCredentials:true});
+            setIsLoading(false);
+            setAllMessages((prev)=>{
+                return [...(prev || []), {
+                    content: response.data.reply,
+                    createdAt:Date.now(),
+                    role: "assistant"
+                }]
+            })
+        }catch(err){
+            console.log(err.data.messages)
+            setIsLoading(false);
+        }
+    }
+    
     useEffect(()=>{
-            console.log(messages)
-    },[messages])
+        messageEndRef.current?.scrollIntoView({behavior:"smooth"}) ;   
+    },[Allmessages])
+    
     useEffect(()=>{
+        if(!chatId)return
         const getmessages=async ()=>{
         try{
+            const url = "http://localhost:3000/api/chat/all-message-in-conversation/" + chatId
             const response=await axios.get(url,{withCredentials:true})
-            setMessages(response.data.messages)
+            setAllMessages(response.data.messages)
         }catch(err){
             console.log(err.response)
             alert("Could not fetch conversation")
@@ -25,18 +63,52 @@ const ChatPage = ({params}) => {
         getmessages()
     },[chatId])
 
-    const handlepost=()=>{
-
-    }
+    
   return (
-    <div className='flex h-screen w-full bg-amber-300'>
-      <Sidebar className='hidden h-full md:block w-[256px] shrink-0 border-r-2 scroll-auto'/>
+    <div className='flex h-screen w-full'>
+        <Sidebar className='hidden h-full md:block w-[256px] shrink-0 border-r-2 scroll-auto'/>
         <div className='h-full w-full flex flex-col items-center'>
-            <div className='w-full px-8 py-4 text-xl bg-amber-100'>
+            <div className='w-full px-8 py-4 text-xl'>
                 ChatBot App
             </div>
-            <div className='h-full flex flex-col w-full md:w-[512px] lg:w-[640px] xl:w-[768px] lg:w- bg-amber-700 '>
-                
+            <div className='flex-1 flex flex-col w-full md:w-[512px] lg:w-[640px] xl:w-[768px] overflow-y-auto pt-4'>
+                <div className='flex flex-col mt-auto w-full'>
+                {
+                    Allmessages.map((message,index)=>{
+                        return(<div key={index} className='w-full '>
+                            {
+                                message.role=="user"?(
+                                    <div className='mt-[32px] w-fit ml-auto rounded-2xl p-[4px] px-[16px] mb-[32px] bg-[#E5E4E2]'>
+                                        {message.content}
+                                    </div>
+                                ):(
+                                    
+                                    <div>
+                                        <ReactMarkdown>
+                                            {message.content}
+                                        </ReactMarkdown>
+                                        <div className='px-[64px] mt-[32px] mb-[32px]'>
+                                            <div className='border-b border-[#808080]' ></div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        </div>)
+                    })
+                }
+                {
+                isLoading&&(<div className='animate-pulse'>
+                            Assistant is Typing...
+                            <div className='px-[64px] mt-[32px]'>
+                            </div>
+                        </div>)
+                }
+                <div ref={messageEndRef}/>
+                </div>
+            </div>
+            <Chatbox className='border-2 w-full md:w-[512px] lg:w-[640px] xl:w-[768px] min-h-[40px] rounded-4xl p-[10px] flex' onSend={handleSend}/>
+            <div className='pb-[4px] text-xs md:text-sm text-[#808080]'>
+                ChatBot App can make mistakes. Check important info.
             </div>
         </div>
     </div>
--- frontend/components/Chatbox.jsx
+++ frontend/components/Chatbox.jsx
@@ -9,14 +9,20 @@ const Chatbox = ({className,onSend}) => {
       setMessage(e.target.value);
   }
 
-  const handleSubmit=()=>{
+  const handleSubmit=(e)=>{
     onSend(message)
+    setMessage("")
   }
   return (
     <div className={`${className}`}>
       <div className='h-full flex items-center px-[10px] w-full'>
         <LuPlus className='text-[#555555] h-[36px] w-[36px] p-[4px]  rounded-[50%] hover:bg-[#f9f8f8]'/>
-        <TextareaAutosize minRows={1} maxRows={10} className='focus:outline-none resize-none p-2 ml-[8px] w-full' placeholder='Ask Anything' onInput={handleInput} />
+        <TextareaAutosize minRows={1} maxRows={10} className='focus:outline-none resize-none p-2 ml-[8px] w-full' placeholder='Ask Anything' value={message} onChange={handleInput} onKeyDown={(e)=>{
+          if(e.key==="Enter" && !e.shiftKey){
+            e.preventDefault();
+            handleSubmit(e);
+          }
+        }}/>
         <FaArrowUp className='h-[36px] w-[38px] p-[8px] rounded-[50%] text-white bg-black' onClick={handleSubmit}/>
       </div>
     </div>
