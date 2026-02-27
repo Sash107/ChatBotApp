162cc0b8dc83207953a80552e4582b2f67c0d5eb
Sash107 <suyashupadhyay2004@gmail.com>
2026-02-27 10:18:19 +0530

    update

--- frontend/app/chat/page.jsx
+++ frontend/app/chat/page.jsx
@@ -12,14 +12,14 @@ const chats = () => {
     setMessage(data)
   }
   return (
-    <div className='flex h-screen w-full bg-amber-300 border-2'>
+    <div className='flex h-screen w-full border-2'>
       <Sidebar className='hidden h-full md:block w-[260px] shrink-0 border-r-2 scroll-auto'/>
       <div className='h-full flex-1 flex flex-col'>
         <div className='px-8 py-4 text-xl'>
           ChatBot App
         </div>
 
-        <div className='flex h-full flex-1 items-center justify-center bg-amber-800'>
+        <div className='flex h-full flex-1 items-center justify-center'>
           <div className='flex-1 flex flex-col translate-y-[-116px]'>
             <p className='text-3xl flex justify-center py-[32px]'>
             What are you working on?
--- frontend/components/Chatbox.jsx
+++ frontend/components/Chatbox.jsx
@@ -16,14 +16,14 @@ const Chatbox = ({className,onSend}) => {
   return (
     <div className={`${className}`}>
       <div className='h-full flex items-center px-[10px] w-full'>
-        <LuPlus className='text-[#555555] h-[36px] w-[36px] p-[4px]  rounded-[50%] hover:bg-[#f9f8f8]'/>
+        <LuPlus className='text-[#555555] h-[36px] w-[36px] p-[4px]  rounded-[50%] hover:bg-[#f9f8f8] hover:cursor-pointer'/>
         <TextareaAutosize minRows={1} maxRows={10} className='focus:outline-none resize-none p-2 ml-[8px] w-full' placeholder='Ask Anything' value={message} onChange={handleInput} onKeyDown={(e)=>{
           if(e.key==="Enter" && !e.shiftKey){
             e.preventDefault();
             handleSubmit(e);
           }
         }}/>
-        <FaArrowUp className='h-[36px] w-[38px] p-[8px] rounded-[50%] text-white bg-black' onClick={handleSubmit}/>
+        <FaArrowUp className='h-[36px] w-[38px] p-[8px] rounded-[50%] text-white bg-black hover:cursor-pointer' onClick={handleSubmit}/>
       </div>
     </div>
   )
