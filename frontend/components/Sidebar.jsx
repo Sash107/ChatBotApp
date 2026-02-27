30c930e6cec8bc158dc5405eacb4b9e15d661460
Sash107 <suyashupadhyay2004@gmail.com>
2026-02-27 13:18:21 +0530

    done with sidebar

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import axios, { all } from 'axios';
 import React, { useEffect, useState } from 'react'
 import { PiNotePencil } from "react-icons/pi";
 import { useParams, useRouter } from 'next/navigation';
+import { FiSidebar } from "react-icons/fi";
 
 const Sidebar = ({className}) => {
   const {chatId}=useParams();
@@ -26,11 +27,13 @@ const Sidebar = ({className}) => {
 
   return (
     <div className={`flex flex-col bg-[#f9f8f8] border-[#E5E4E2] ${className}`}>
-        <div className='h-[64px]' >
+        <div className='h-[64px] items-center flex px-[20px]'>
+          <img src="/logo.png" className='w-[40px] hover:cursor-pointer' onClick={()=>router.push("/chat")}/>
+          <FiSidebar size={20} className='text-[#777777] ml-auto hover:cursor-pointer'/>
         </div>
-        <div className='flex-1 flex flex-col border-2 min-h-0'>
+        <div className='flex-1 flex flex-col min-h-0'>
           <div className='px-[4px]'>
-            <div className='w-full p-[8px] mt-[12px] text-md flex items-center gap-[8px] rounded-xl hover:bg-[#eaeaea] hover:cursor-pointer'>
+            <div className='w-full p-[8px] mt-[12px] text-md flex items-center gap-[8px] rounded-xl hover:bg-[#eaeaea] hover:cursor-pointer' onClick={()=> router.push("/chat")}>
               <PiNotePencil size={24}/>
               New Chat
             </div>
@@ -42,9 +45,7 @@ const Sidebar = ({className}) => {
               allConversation.map((conv,index)=>{
                 return(
                   <div className={`w-full h-[40px] p-[8px] pl-[12px] mt-[12px] text-md flex rounded-xl hover:bg-[#eaeaea] hover:cursor-pointer overflow-hidden ${chatId==conv._id?"bg-[#eaeaea] hover:bg-[#ededed]":""}`} onClick={()=>{
-                    const page_id_url="http://localhost:3001/chat/"+conv._id
-                    console.log(page_id_url)
-                    router.push(page_id_url)
+                    router.push(`/chat/${conv._id}`)
                   }}>
                     {conv.title}
                   </div>
